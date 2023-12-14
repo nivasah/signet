4552d4c8d4954a1ec555cd34495341603227cd2d
Alwin Doss <alwindoss84@gmail.com>
2023-12-14 19:29:54 +0530

    working copy
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <h1>Signet</h1>
+        <h2>Bookmark manager</h2>
       </div>
     </>
   )
